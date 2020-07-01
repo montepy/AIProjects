a7931d7b2577ca01648cb18a17dcf2d1ed42ed06
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-06-30 21:04:35 -0400

    added a node class with expand method
    the node class will represent the different positions that the ai will access. don't know if this looks good or bad.

--- AIProjects-master/Project1/RepeatedForwardAStarBreakLarge.py
+++ AIProjects-master/Project1/RepeatedForwardAStarBreakLarge.py
@@ -5,7 +5,45 @@ for i in list(range(101)):  #converts text grid to more easily used array form
     line = grid.readline()
     for s in list(range(101)):
         rgrid[i][s] = (line[s:s+2].rstrip(), False) #(blocked, visited)
+        
+openlist = []
+closedlist = []
 
 heap = []
 def ComputePath(self,rgrid):
     loggrid = [101][101]
+    
+
+    
+#need to create closedlists and open list. 
+#open list is a priority que that organizes lists by nodes which represents the 
+
+class node:
+    def __init__(self, name, x, y, costToGo, costToCome,parent):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.costToGo = costToGo
+        self.costToCome = costToCome
+        self.parent = parent
+
+    def expand()
+        for i in closedlist:
+            if (this.x == i.x and this.y == i.y):
+                openlist.remove(this)
+                #closedlist.append(this) #doesn't really need to be in the closed list, only takes up space
+                return
+            
+        #need code to find the cost to come
+        n = node(this.name + "_north", this.y-1, this.x, this.costToGo+1, *COSTTOCOMEVAR, this)
+        openlist.append(n)
+        s = node(this.name + "_south", this.y+1, this.x, this.costToGo+1, *COSTTOCOMEVAR, this)
+        openlist.append(n)
+        e = node(this.name + "_east", this.y, this.x+1, this.costToGo+1, *COSTTOCOMEVAR, this)
+        openlist.append(n)
+        w = node(this.name + "_west", this.y, this.x-1, this.costToGo+1, *COSTTOCOMEVAR, this)
+        openlist.append(n)
+        
+        openlist.remove(this)
+        closedlist.append(this)
+        return
