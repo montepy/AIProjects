22ded6df8401124b158b56ace525b43651302899
Edward Wang <epywang42gmail.com>
2020-07-02 12:57:25 -0400

    heap class made. Will edit later as needed.

---
+++ AIProjects-master/Project1/BreakLargeHeap.py
@@ -0,0 +1,67 @@
+import sys
+
+class BLHeap:
+
+    def __init__(self,maxsize):
+        self.maxsize = maxsize
+        self.size = 0
+        self.array = []
+        
+    def getMin(self):
+        return self.array[0]
+
+    def removeMin(self):
+        out = self.array[0]
+        i = 0
+        while i < len(self.array):
+            if self.array[i*2+1].fvalue() < self.array[i*2+2].fvalue():
+                self.array[i] = self.array[i*2+1]
+                i = i*2+1
+            else if self.array[i*2+1].fvalue() == self.array[i*2+2].fvalue():
+                if self.array[i*2+1].costToGo > self.array[i*2+2].costToGo:
+                    self.array[i] = self.array[i*2+1]
+                    i = i*2+1
+                else self.array[i*2+1].costToGo < self.array[i*2+2].costToGo:
+                    self.array[i] = self.array[i*2+2]
+                    i = i*2+2
+            else:
+                self.array[i] = self.array[i*2+2]
+                i = i*2+2
+            
+        return out
+    
+    def insert(self, node):
+        self.array.append(node)
+        i = len(self.array)-1
+        while node.fvalue() < self.array[(i-1)/2].fvalue() and i > 0:
+            self.array[i] = self.array[(i-1)/2]
+            i = (i-1)/2
+            self.array[i] = node
+        if node.fvalue() == self.array[(i-1)/2].fvalue() and i > 0:
+            if node.costToGo > self.array[i-1/2].costToGo:
+                self.array[i] = self.array[(i-1)/2]
+                i = (i-1)/2
+                self.array[i] = node
+
+        return
+    
+    def delete(self,index):
+        i = index
+        self.array[i].costToGo = -10000
+        while self.array[i].fvalue() < self.array[(i-1)/2].fvalue() and i > 0:
+            temp = self.array[i]
+            self.array[i] = self.array[(i-1)/2]
+            i = (i-1)/2
+            self.array[i] = temp
+        if self.array[i].fvalue() == self.array[(i-1)/2].fvalue() and i > 0:
+            if self.array[i].costToGo > self.array[i-1/2].costToGo:
+                temp = self.array[i]
+                self.array[i] = self.array[(i-1)/2]
+                i = (i-1)/2
+                self.array[i] = temp
+        removeMin()
+        return
+
+
+
+
