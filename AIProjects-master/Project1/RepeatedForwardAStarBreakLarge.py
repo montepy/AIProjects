c5ac17fde42ea46324b6496b407bccadb99637dc
kyle4036 <56771288+kyle4036@users.noreply.github.com>
2020-07-02 14:07:21 -0400

    removed the name attribute from the node class

--- AIProjects-master/Project1/RepeatedForwardAStarBreakLarge.py
+++ AIProjects-master/Project1/RepeatedForwardAStarBreakLarge.py
@@ -14,11 +14,10 @@ def ComputePath(self,rgrid):
     loggrid = [101][101]
     
 
-#openlist is a priority que that organizes lists by nodes which represents the 
 
 class node:
-    def __init__(self, name, x, y, costToGo, parent):
-        self.name = name
+    def __init__(self, x, y, costToGo, parent):
+        #self.name = name
         self.x = x
         self.y = y
         self.costToGo = costToGo
@@ -40,15 +39,14 @@ class node:
                 openlist.remove(self)
                 #closedlist.append(self) #doesn't really need to be in the closed list, only takes up extra space
                 return
-            
-        #need code to find the cost to come
-        n = node(self.name + "_north", self.y-1, self.x, self.costToGo+1, self)
+        
+        n = node(self.y-1, self.x, self.costToGo+1, self)
         openlist.append(n)
-        s = node(self.name + "_south", self.y+1, self.x, self.costToGo+1, self)
+        s = node(self.y+1, self.x, self.costToGo+1, self)
         openlist.append(s)
-        e = node(self.name + "_east", self.y, self.x+1, self.costToGo+1, self)
+        e = node(self.y, self.x+1, self.costToGo+1, self)
         openlist.append(e)
-        w = node(self.name + "_west", self.y, self.x-1, self.costToGo+1, self)
+        w = node(self.y, self.x-1, self.costToGo+1, self)
         openlist.append(w)
         
         openlist.remove(self)
